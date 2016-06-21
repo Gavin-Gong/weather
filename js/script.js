74199bf98a06f564d1de6b92dd428ba8b289962b
Gavin Gong <kingdompluto@gmail.com>
2016-06-21 11:35:03 +0800

    basic function completed

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 // get and store data by ajax
-var Data = {}
 var apiURL = 'http://apis.baidu.com/heweather/weather/free?city='
-// var apiURL = 'https://api.heweather.com/x3/weather?cityid='
+    // var apiURL = 'https://api.heweather.com/x3/weather?cityid='
+    // 设置星期排序数组
+var Data = {}
+convertWeekList()
+
+function convertWeekList(weeklist) {
+    var weeklist = ['周日', '周一', '周二', '周三', '周四', '周五', '周六']
+    var date = new Date()
+    var nowWeek = date.getDay() + 1
+    var arr_1 = weeklist.slice(0, nowWeek)
+    var arr_2 = weeklist.slice(nowWeek)
+    Data.WeekList = arr_2.concat(arr_1)
+}
+
+
 $('.search-bar').bind('focus', function() {
     $.ajax({
         type: "GET",
@@ -20,7 +33,8 @@ $('.search-bar').bind('focus', function() {
                 var hourlyForecast = responseData.hourly_forecast
                 var suggestion = responseData.suggestion
                 var status = responseData.status
-                console.log(suggestion)
+                var now = responseData.now
+                console.log(responseData)
 
                 var maxTempList = []
                 var minTempList = []
@@ -28,7 +42,10 @@ $('.search-bar').bind('focus', function() {
                 var nightWeatherList = []
                 var dayWeatherCodes = []
                 var nightWeatherCodes = []
-                // format data as need
+                var air = aqi.city.aqi + '|' + aqi.city.qlty
+                var nowTemp = now.tmp
+                var nowWeather = now.cond.txt
+                    // format data as need
                 for (var i = 0; i < dailyForecast.length; i++) {
                     maxTempList.push(dailyForecast[i].tmp.max)
                     minTempList.push(dailyForecast[i].tmp.min)
@@ -43,139 +60,151 @@ $('.search-bar').bind('focus', function() {
                 Data.nightWeatherList = nightWeatherList
                 Data.nightWeatherCodes = nightWeatherCodes
                 Data.dayWeatherCodes = dayWeatherCodes
+                Data.nowTemp = nowTemp
+                Data.nowWeather = nowWeather
+                Data.air = air
                 console.log(Data)
             }
         }
     })
 })
 $('.search-bar').trigger('focus')
-    
-// Data.maxTempList = [23, 45, 12, 35, 29, 33, 8]
-// Data.minTempList = [29, 35, 22, 15, 29, 23, 18]
-// Data.dayWeatherList = ['cloud', 'cloud', 'cloud', 'cloud', 'cloud', 'cloud', 'cloud']
-// Data.nightWeatherList = ['cloud', 'cloud', 'cloud', 'cloud', 'cloud', 'cloud', 'cloud']
 
-// 设置星期排序数组
-convertWeekList()
-function convertWeekList(weeklist) {
-	var weeklist = ['周日','周一', '周二', '周三', '周四', '周五', '周六']
-	var date = new Date()
-	var nowWeek = date.getDay()
-	var arr_1 = weeklist.slice(0,nowWeek)
-	var arr_2 = weeklist.slice(nowWeek)
-	Data.WeekList = arr_2.concat(arr_1) 
-}
+
 // canvas
 $(document).ajaxSuccess(function() {
-// tempertature line
-var templine = $('#templine')[0];
-console.log(typeof templine)
-if (templine.getContext) {
-    templine.width = window.innerWidth
-    templine.height = 400
-    var intervalWidth = Math.floor((templine.width-80) / 6) // 点与点之间的距离
-    var ctx = templine.getContext('2d')
-    console.log(ctx)
-    ctx.strokeStyle = '#eee'
-    ctx.lineWidth = 2
-    drawUpperLine(intervalWidth, Data.maxTempList)
+    // tempertature line
+    fillWeather(Data)
+    drawWeatherCanvas('templine', Data)
 
-    drawUnderLine(intervalWidth, Data.minTempList)
-
-    darwUpperDots(intervalWidth, Data.maxTempList)
-
-    drawUnderDots (intervalWidth, Data.minTempList)
-
-    drawWeatherText (intervalWidth, Data.dayWeatherList, Data.WeekList)
-
-    drawWeatherIcon(intervalWidth, Data.dayWeatherCodes)
-
-}
-
-function drawUpperLine(width, data) {
-    var temp = 40
-    ctx.beginPath()
-    ctx.moveTo(40, data[0] * 4)
-    for (var g = 0; g < data.length; g++) {
-        ctx.lineTo(temp, data[g] * 4)
-        ctx.fillStyle = '#fff'
-        ctx.font = '14px'
-        ctx.textAlign = "center"
-        ctx.fillText(data[g], temp, data[g] * 4 - 15)
-        temp += width
-    }
-    ctx.stroke()
-}
-
-function drawUnderLine(width, data) {
-    var temp = 40
-    ctx.beginPath()
-    ctx.moveTo(40, data[0] * 4 + 100)
-    for (var g = 0; g < data.length; g++) {
-        ctx.lineTo(temp, data[g] * 4 + 100)
-        ctx.fillStyle = '#fff'
-        ctx.font = '14px'
-        ctx.textAlign = "center"
-        ctx.fillText(data[g], temp, data[g] * 4 + 25 + 100)
-        temp += width
-    }
-    ctx.stroke()
-}
-
-function darwUpperDots(width, data) {
-	var temp = 40
-	ctx.beginPath()
-	for(var k=0; k<data.length; k++) {
-		ctx.moveTo(temp, data[k]*4)
-		ctx.arc(temp, data[k]*4, 5, 0, 2 * Math.PI)
-		temp+= width
-	}
-	ctx.fill()
-}
-
-function drawUnderDots (width, data) {
-	var temp = 40
-	ctx.beginPath()
-	for(var k=0; k<data.length; k++) {
-		ctx.moveTo(temp, data[k]*4+100)
-		ctx.arc(temp, data[k]*4+100, 5, 0, 2 * Math.PI)
-		temp+= width
-	}
-	ctx.fill()
-}
-
-function drawWeatherText (width, dataWeather, dataWeek) {
-	var temp = 40
-
-	ctx.fillStyle = '#fff'
-    ctx.font = '14px 华文细黑'
-    ctx.textAlign = "center"
-    ctx.beginPath()
-	for(var d=0; d<dataWeather.length; d++) {
-		ctx.fillText(dataWeek[d], temp, 300)
-        ctx.fillText(dataWeather[d], temp, 330)
-		temp+=width
-	}
-}
-
-function drawWeatherIcon(width, IconCodes) {
-    var temp = 50
-	var baseIconUrl = 'http://www.heweather.com/weather/images/icon/' //加载天气IOCN
-	var imagesObjectArr = new Array()
-	for(var p = 0; p < IconCodes.length; p++) {
-		imagesObjectArr[p] = new Image()
-		imagesObjectArr[p].src = baseIconUrl + IconCodes[p] + '.png'
-		imagesObjectArr[p].onload = (function () {
-            console.log(imagesObjectArr[p])
-			ctx.drawImage(imagesObjectArr[p], temp*4 - 120, 1130, 100, 100)
-		})()
-        temp += (width - 10)
-	}
-}
 
 })
 
-// disable right-click menu
-// $(document).bind('contextmenu', function() {
-//     return false
-// })
+function drawWeatherCanvas(canvasId, Data) {
+    //const
+    var STARTx = 40
+
+    var templine = document.getElementById('templine')
+    templine.width = window.innerWidth
+    templine.height = 500
+    var intervalWidth = Math.floor((templine.width - 2 * STARTx) / 6) // 点与点之间的距离
+    console.log(intervalWidth)
+    var ctx = templine.getContext('2d')
+    var intervalWidth_1 = Math.floor((templine.width - 2 * STARTx) / 6) // why not same as intervalWidth?
+    console.log(intervalWidth_1)
+
+
+    // canvas style
+    ctx.strokeStyle = '#eee'
+    ctx.lineWidth = 2
+
+    if (templine.getContext) {
+        drawUpperLine(Data.maxTempList)
+
+        drawUnderLine(Data.minTempList)
+
+        darwUpperDots(Data.maxTempList)
+
+        drawUnderDots(Data.minTempList)
+
+        drawWeatherText(Data.dayWeatherList, Data.WeekList)
+
+        drawWeatherIcon(Data.dayWeatherCodes)
+
+    }
+
+    function drawUpperLine(data) {
+        var temp = STARTx
+        ctx.beginPath()
+        ctx.moveTo(40, data[0] * 4)
+        for (var g = 0; g < data.length; g++) {
+            ctx.lineTo(temp, data[g] * 4)
+            ctx.fillStyle = '#fff'
+            ctx.font = '12px'
+            ctx.textAlign = "center"
+            ctx.fillText(data[g]+ '°', temp, data[g] * 4 - 15)
+            temp += intervalWidth
+        }
+        ctx.stroke()
+    }
+
+    function drawUnderLine(data) {
+        var temp = STARTx
+        ctx.beginPath()
+        ctx.moveTo(40, data[0] * 4 + 100)
+        for (var g = 0; g < data.length; g++) {
+            ctx.lineTo(temp, data[g] * 4 + 100)
+            ctx.fillStyle = '#fff'
+            ctx.font = '12px'
+            ctx.textAlign = "center"
+            ctx.fillText(data[g]+ '°', temp, data[g] * 4 + 25 + 100)
+            temp += intervalWidth
+        }
+        ctx.stroke()
+    }
+
+    function darwUpperDots(data) {
+        var temp = STARTx
+        ctx.beginPath()
+        for (var k = 0; k < data.length; k++) {
+            ctx.moveTo(temp, data[k] * 4)
+            ctx.arc(temp, data[k] * 4, 5, 0, 2 * Math.PI)
+            temp += intervalWidth
+        }
+        ctx.fill()
+    }
+
+    function drawUnderDots(data) {
+        var temp = STARTx
+        ctx.beginPath()
+        for (var k = 0; k < data.length; k++) {
+            ctx.moveTo(temp, data[k] * 4 + 100)
+            ctx.arc(temp, data[k] * 4 + 100, 5, 0, 2 * Math.PI)
+            temp += intervalWidth
+        }
+        ctx.fill()
+    }
+
+    function drawWeatherText(dataWeather, dataWeek) {
+        var temp = STARTx
+        ctx.fillStyle = '#fff'
+        ctx.font = '12px 华文细黑'
+        ctx.textAlign = "center"
+        ctx.beginPath()
+        for (var d = 0; d < dataWeather.length; d++) {
+            ctx.fillText(dataWeek[d], temp, 300)
+            ctx.fillText(dataWeather[d], temp, 330)
+            temp += intervalWidth
+        }
+    }
+
+    function drawWeatherIcon(IconCodes) {
+        var temp = STARTx + 10
+        var baseIconUrl = 'http://www.heweather.com/weather/images/icon/' //加载天气IOCN
+        var imagesObjectArr = new Array()
+        for (var p = 0; p < IconCodes.length; p++) {
+            imagesObjectArr[p] = new Image()
+            imagesObjectArr[p].src = baseIconUrl + IconCodes[p] + '.png'
+            imagesObjectArr[p].onload = (function() {
+                console.log(imagesObjectArr[p])
+                // ctx.drawImage(imagesObjectArr[p], temp * 4 - 110, 1130, 100, 100)
+                ctx.drawImage(imagesObjectArr[p], temp, 1130, 100, 100)
+            })()
+            temp += (intervalWidth + 145)
+        }
+    }
+}
+
+
+
+
+function fillWeather(WeatherData) {
+    var tempEle = $('.temperature')
+    var weatherEle = $('.weather')
+    var airEle = $('.air')
+    tempEle.text(WeatherData.nowTemp + '°')
+    console.log(WeatherData.nowTemp)
+    weatherEle.text(WeatherData.nowWeather)
+    airEle.text(WeatherData.air) 
+    console.log(WeatherData.nowWeather)
+}
